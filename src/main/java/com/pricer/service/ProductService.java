64e671e450a2f1ff38be8267b92b60185686414e
Indranil Sinha <indrasinha84@gmail.com>
2018-06-05 22:37:19 +0530

    Pushing ProductService before generic changes for service layer.

--- src/main/java/com/pricer/service/ProductService.java
+++ src/main/java/com/pricer/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.pricer.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Service;
 
 import com.pricer.entity.Product;
 import com.pricer.entity.RESTMessage;
+import com.pricer.entity.sequence.ProductWithSequence;
 import com.pricer.repository.ProductRepository;
+import com.pricer.repository.ProductWithSequenceRepository;
 import com.pricer.rest.dto.ProductRequestDTO;
 import com.pricer.rest.dto.ProductResponseDTO;
 import com.pricer.rest.dto.RESTResponse;
@@ -19,22 +22,23 @@ import com.pricer.rest.exception.ResourceNotFoundException;
 public class ProductService {
 
 	@Autowired
+	ProductWithSequenceRepository productWithSequenceRepository;
+
+	@Autowired
 	ProductRepository productRepository;
 
 	public RESTResponse<ProductResponseDTO> addProduct(ProductRequestDTO product) {
-		Product createdProduct = productRepository.save(product.toEntity());
+		ProductWithSequence createdProduct = productWithSequenceRepository.save(product.toEntityWithSequence());
 		ProductResponseDTO productResponseDTO = new ProductResponseDTO();
-		productResponseDTO.buildResponse(createdProduct);
+		productResponseDTO.buildResponseUsingSequence(createdProduct);
 		RESTResponse<ProductResponseDTO> response = new RESTResponse<>(HttpStatus.OK, RESTMessage.OK,
 				productResponseDTO);
 		return response;
 	}
 
 	public RESTResponse<ProductResponseDTO> updateProduct(ProductRequestDTO product, Integer id) {
-		
-		Product productToBeUpdated = product.toEntity();
-		productToBeUpdated.setId(id);
 
+		Product productToBeUpdated = product.toEntity(id);
 		Product updatedProduct = productRepository.save(productToBeUpdated);
 		ProductResponseDTO productResponseDTO = new ProductResponseDTO();
 		productResponseDTO.buildResponse(updatedProduct);
@@ -44,18 +48,38 @@ public class ProductService {
 	}
 
 	public RESTResponse<ProductResponseDTO> retriveProduct(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Product> productOptional = productRepository.findById(id);
+		if (productOptional.isPresent()) {
+			ProductResponseDTO productResponseDTO = new ProductResponseDTO();
+			productResponseDTO.buildResponse(productOptional.get());
+			RESTResponse<ProductResponseDTO> response = new RESTResponse<>(HttpStatus.OK, RESTMessage.OK,
+					productResponseDTO);
+			return response;
+		} else {
+			throw new ResourceNotFoundException("Product", "id", id);
+		}
 	}
 
 	public RESTResponse<List<ProductResponseDTO>> listProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> products = productRepository.findAll();
+		List<ProductResponseDTO> productResponseDTOs = new ArrayList<>(products.size());
+		products.stream()
+				.forEach(product -> 
+						{
+							ProductResponseDTO newDTO = new ProductResponseDTO();
+							newDTO.buildResponse(product);
+							productResponseDTOs.add(newDTO);
+						});
+		RESTResponse<List<ProductResponseDTO>> response = new RESTResponse<>(HttpStatus.OK, RESTMessage.OK,
+				productResponseDTOs);
+		return response;
 	}
 
-	public RESTResponse<ProductResponseDTO> deleteProduct(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public RESTResponse<String> deleteProduct(Integer id) {
+		productRepository.deleteById(id);
+		RESTResponse<String> response = new RESTResponse<>(HttpStatus.OK, RESTMessage.OK,
+				"");
+		return response;
 	}
 
 }
