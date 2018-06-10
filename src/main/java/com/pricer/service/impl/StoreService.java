597efb5b882852aa5352865699e9905d93b4923e
Indranil Sinha <indrasinha84@gmail.com>
2018-06-11 03:51:24 +0530

    Missed Annnotation

--- src/main/java/com/pricer/service/impl/StoreService.java
+++ src/main/java/com/pricer/service/impl/StoreService.java
@@ -1,8 +1,11 @@
 package com.pricer.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.pricer.model.Store;
 import com.pricer.repository.StoreRepository;
 
+@Service
 public class StoreService extends AbstractCRUDDataAccessService<Store, Integer, StoreRepository> {
 
 	@Override
