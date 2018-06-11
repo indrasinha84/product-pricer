276e62ff45e3c231478f67a1edf3b12eb6e18b85
Indranil Sinha <indrasinha84@gmail.com>
2018-06-11 17:52:18 +0530

    PostGreSQL scripts updated.

--- sql/postgresql/tables.sql
+++ sql/postgresql/tables.sql
@@ -1,4 +1,3 @@
---DROP SCHEMA IF EXISTS "PUBLIC" CASCADE; 
 
 CREATE TABLE product 
   ( 
@@ -6,9 +5,8 @@ CREATE TABLE product
      product_name        VARCHAR(100) NOT NULL, 
      product_description VARCHAR(1000), 
      base_price          NUMERIC, 
-     created_date        TIMESTAMP, 
-     latest_details_id   INTEGER 
-  ); 
+     created_date        TIMESTAMP
+  );
 
 CREATE SEQUENCE seq_product START WITH 1 INCREMENT BY 1; 
 
@@ -34,7 +32,8 @@ CREATE TABLE store_price
      store_id       INTEGER, 
      product_id     INTEGER, 
      product_notes  VARCHAR(1000), 
-     store_price    NUMERIC, 
+     store_price    NUMERIC,
+     eff_sts		VARCHAR(20),
      created_date   TIMESTAMP, 
      FOREIGN KEY (store_id) REFERENCES store(store_id), 
      FOREIGN KEY (product_id) REFERENCES product(product_id) 
@@ -44,19 +43,21 @@ CREATE SEQUENCE seq_store_price START WITH 1 INCREMENT BY 1;
 
 CREATE UNIQUE INDEX ind1_store_price 
   ON store_price (store_price_id); 
+  
+CREATE INDEX ind2_store_price 
+  ON store_price (store_id, product_id, eff_sts);  
 
 CREATE TABLE price_details_log 
   ( 
      details_id          INTEGER PRIMARY KEY, 
      product_id          INTEGER, 
      average_store_price NUMERIC, 
-     lowest_price_id     INTEGER, 
-     highest_price_id    INTEGER, 
+     lowest_price        NUMERIC, 
+     highest_price       NUMERIC, 
      ideal_store_price   NUMERIC, 
      count_of_prices     INTEGER, 
-     created_date        TIMESTAMP, 
-     FOREIGN KEY (lowest_price_id) REFERENCES store_price(store_price_id), 
-     FOREIGN KEY (highest_price_id) REFERENCES store_price(store_price_id), 
+     eff_sts			 VARCHAR(20),
+     created_date        TIMESTAMP,
      FOREIGN KEY (product_id) REFERENCES product(product_id) 
   ); 
 
@@ -65,17 +66,21 @@ CREATE SEQUENCE seq_price_details_log START WITH 1 INCREMENT BY 1;
 CREATE UNIQUE INDEX ind1_price_details_log 
   ON price_details_log (details_id); 
 
-ALTER TABLE product 
-  ADD FOREIGN KEY (latest_details_id) REFERENCES price_details_log(details_id); 
-
+CREATE INDEX ind2_price_details_log 
+  ON price_details_log (product_id, eff_sts); 
+  
 CREATE TABLE price_calculation_request 
   ( 
      calculation_request_id INTEGER UNIQUE, 
-     details_id_start       INTEGER, 
-     details_id_end         INTEGER, 
+     store_price_id_start       INTEGER, 
+     store_price_id_end         INTEGER, 
      requested_date         TIMESTAMP, 
      start_date             TIMESTAMP, 
-     end_date               TIMESTAMP 
+     end_date               TIMESTAMP,
+     store_price_id_restart     INTEGER,
+	 event_type 			VARCHAR(50),
+     job_sts                VARCHAR(50)
+
   ); 
 
 CREATE SEQUENCE seq_price_calculation_request START WITH 1 INCREMENT BY 1; 
@@ -83,6 +88,7 @@ CREATE SEQUENCE seq_price_calculation_request START WITH 1 INCREMENT BY 1;
 CREATE UNIQUE INDEX ind1_price_calculation_request 
   ON price_calculation_request (calculation_request_id); 
 
---- Index on CALCULATION_REQUEST_ID 
+CREATE INDEX ind2_price_calculation_request 
+  ON price_calculation_request (job_sts, store_price_id_end); 
 
 COMMIT; 
