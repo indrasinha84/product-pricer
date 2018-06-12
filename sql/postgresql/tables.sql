8006f2c325e024685099c308f23966c77ce8807b
Indranil Sinha <indrasinha84@gmail.com>
2018-06-12 06:13:45 +0530

    DB Scripts updated.

--- sql/postgresql/tables.sql
+++ sql/postgresql/tables.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE product 
+CREATE TABLE "PUBLIC".product 
   ( 
      product_id          INTEGER PRIMARY KEY, 
      product_name        VARCHAR(100) NOT NULL, 
@@ -8,12 +8,12 @@ CREATE TABLE product
      created_date        TIMESTAMP
   );
 
-CREATE SEQUENCE seq_product START WITH 1 INCREMENT BY 1; 
+CREATE SEQUENCE "PUBLIC".seq_product START WITH 1 INCREMENT BY 1; 
 
 CREATE UNIQUE INDEX ind1_product 
-  ON product (product_id); 
+  ON "PUBLIC".product (product_id); 
 
-CREATE TABLE store 
+CREATE TABLE "PUBLIC".store 
   ( 
      store_id          INTEGER PRIMARY KEY, 
      store_name        VARCHAR(100), 
@@ -21,12 +21,12 @@ CREATE TABLE store
      created_date      TIMESTAMP 
   ); 
 
-CREATE SEQUENCE seq_store START WITH 1 INCREMENT BY 1; 
+CREATE SEQUENCE "PUBLIC".seq_store START WITH 1 INCREMENT BY 1; 
 
 CREATE UNIQUE INDEX ind1_store 
-  ON store (store_id); 
+  ON "PUBLIC".store (store_id); 
 
-CREATE TABLE store_price 
+CREATE TABLE "PUBLIC".store_price 
   ( 
      store_price_id INTEGER PRIMARY KEY, 
      store_id       INTEGER, 
@@ -35,19 +35,19 @@ CREATE TABLE store_price
      store_price    NUMERIC,
      eff_sts		VARCHAR(20),
      created_date   TIMESTAMP, 
-     FOREIGN KEY (store_id) REFERENCES store(store_id), 
-     FOREIGN KEY (product_id) REFERENCES product(product_id) 
+     FOREIGN KEY (store_id) REFERENCES "PUBLIC".store(store_id), 
+     FOREIGN KEY (product_id) REFERENCES "PUBLIC".product(product_id) 
   ); 
 
-CREATE SEQUENCE seq_store_price START WITH 1 INCREMENT BY 1; 
+CREATE SEQUENCE "PUBLIC".seq_store_price START WITH 1 INCREMENT BY 1; 
 
 CREATE UNIQUE INDEX ind1_store_price 
-  ON store_price (store_price_id); 
+  ON "PUBLIC".store_price (store_price_id); 
   
 CREATE INDEX ind2_store_price 
-  ON store_price (store_id, product_id, eff_sts);  
+  ON "PUBLIC".store_price (store_id, product_id, eff_sts);  
 
-CREATE TABLE price_details_log 
+CREATE TABLE "PUBLIC".price_details_log 
   ( 
      details_id          INTEGER PRIMARY KEY, 
      product_id          INTEGER, 
@@ -58,18 +58,18 @@ CREATE TABLE price_details_log
      count_of_prices     INTEGER, 
      eff_sts			 VARCHAR(20),
      created_date        TIMESTAMP,
-     FOREIGN KEY (product_id) REFERENCES product(product_id) 
+     FOREIGN KEY (product_id) REFERENCES "PUBLIC".product(product_id) 
   ); 
 
-CREATE SEQUENCE seq_price_details_log START WITH 1 INCREMENT BY 1; 
+CREATE SEQUENCE "PUBLIC".seq_price_details_log START WITH 1 INCREMENT BY 1; 
 
 CREATE UNIQUE INDEX ind1_price_details_log 
-  ON price_details_log (details_id); 
+  ON "PUBLIC".price_details_log (details_id); 
 
 CREATE INDEX ind2_price_details_log 
-  ON price_details_log (product_id, eff_sts); 
+  ON "PUBLIC".price_details_log (product_id, eff_sts); 
   
-CREATE TABLE price_calculation_request 
+CREATE TABLE "PUBLIC".price_calculation_request 
   ( 
      calculation_request_id INTEGER UNIQUE, 
      store_price_id_start       INTEGER, 
@@ -77,18 +77,18 @@ CREATE TABLE price_calculation_request
      requested_date         TIMESTAMP, 
      start_date             TIMESTAMP, 
      end_date               TIMESTAMP,
-     store_price_id_restart     INTEGER,
-	 event_type 			VARCHAR(50),
+     store_price_id_restart INTEGER,
+	   event_type 			      VARCHAR(50),
      job_sts                VARCHAR(50)
 
   ); 
 
-CREATE SEQUENCE seq_price_calculation_request START WITH 1 INCREMENT BY 1; 
+CREATE SEQUENCE "PUBLIC".seq_price_calculation_request START WITH 1 INCREMENT BY 1; 
 
 CREATE UNIQUE INDEX ind1_price_calculation_request 
-  ON price_calculation_request (calculation_request_id); 
+  ON "PUBLIC".price_calculation_request (calculation_request_id); 
 
 CREATE INDEX ind2_price_calculation_request 
-  ON price_calculation_request (job_sts, store_price_id_end); 
+  ON "PUBLIC".price_calculation_request (job_sts, store_price_id_end); 
 
 COMMIT; 
